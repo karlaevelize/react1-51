de27e48837ce7e974909a4fa8cd29c29312add81
karlaevelize <k.evelize@gmail.com>
2021-05-03 16:03:27 +0200

    render students and professors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Title from "./components/Title"
+import Hogwarts from "./components/Hogwarts"
 
 function App() {
   return (
     <div className="App">
       <p>My first React app</p>
       <Title title="A list of Hogwarts Students and Professors" description="Not a very commplete list :)"/>
+      <Hogwarts/>
     </div>
   );
 }
---
+++ src/components/Hogwarts.js
@@ -0,0 +1,34 @@
+import data from "../data.json"
+
+export default function Hogwarts(){
+
+  const students = data.students
+  const professors = data.professors
+
+  console.log("students", students)
+  console.log("professors", professors)
+
+  return (
+    <div>
+      <h3>Students</h3>
+      {students.map((student, i) => {
+        return (
+          <div key={student.id}> 
+            <p></p>
+            <h5>{student.firstName} {student.lastName}</h5>
+            <p>{student.house}</p>
+            <p>{student.playsQuidditch ? "yes" : "no"}</p>
+          </div>
+        )
+      })}
+      <h3>Professors</h3>
+      {professors.map((professor, index) => {
+        return (
+          <div key={index}>
+            <p>{professor.name}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
